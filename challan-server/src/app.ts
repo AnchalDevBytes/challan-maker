e47cf79e1c351b1c1d72934c14dd757f7fde318c
anchalraj31082004 <anchalraj20045@gmail.com>
2026-02-22 16:33:40 +0530

    change cors things

--- challan-server/src/app.ts
+++ challan-server/src/app.ts
@@ -6,28 +6,9 @@ import cors from "cors";
 
 const app: Application = express();
 
-const allowedOrigin = process.env.FRONTEND_URL;
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || origin === allowedOrigin) {
-        callback(null, true);
-      } else {
-        console.log("Rejected Origin by CORS:", origin);
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
-);
-
-app.options("/*splat", cors());
-
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is healthy and running!");
